c18e2631ea64e48f8ab11fb3cc2d13cec6e6ca18
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2020-12-25 15:56:15 +0100

    Added DTO's & Repository for Checklistitem

---
+++ RestApi/Profiles/CheckListItemProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using RestApi.Dtos;
+using TravelListModels;
+
+namespace TravelList.Api.Profiles
+{
+    public class CheckListItemProfile : Profile
+    {
+        public CheckListItemProfile()
+        {
+            //Source -> Target
+            CreateMap<CheckListItem, ChecklistItemReadDto>();
+            CreateMap<CheckListItemCreateDto, CheckListItem>();
+        }
+    }
+}
---
+++ TravelListRepository/Sql/SqlCheckListItemRepo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelListRepository.Sql
+{
+    public class SqlCheckListItemRepo : ICheckListItemRepo
+    {
+    }
+}
