a0c7e493a4125702bd043c8735ab95e23e14f8a2
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-01-12 20:28:50 +0100

    Added TaskListItem to models

---
+++ TravelListApp/Models/TaskListItem.cs
@@ -0,0 +1,11 @@
+﻿
+using TravelListModels;
+
+namespace TravelListApp.Models
+{
+    class TaskListItem : TravelTaskListItem
+    {
+        public bool IsNew { get; set; }
+        public bool ToRemove { get; set; }
+    }
+}
---
+++ TravelListModels/TravelTaskListItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelListModels
+{
+    public class TravelTaskListItem
+    {
+        public int TravelTaskListItemID { get; set; }
+        public string Name { get; set; }
+        public bool Checked { get; set; }
+        public int TravelListItemID { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
