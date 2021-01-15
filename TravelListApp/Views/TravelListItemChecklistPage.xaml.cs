866fddcf02b818e7e30503797c1b27a2460b2e17
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-01-15 20:06:27 +0100

    Changed max height on checklist

--- TravelListApp/Views/TravelListItemChecklistPage.xaml.cs
+++ TravelListApp/Views/TravelListItemChecklistPage.xaml.cs
@@ -8,6 +8,7 @@ using TravelListModels;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -38,7 +39,7 @@ namespace TravelListApp.Views
            
 
         }
-
+              
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
